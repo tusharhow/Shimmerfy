cbb869167b852c5b2398407d8057d7a1da8d5f4e
tusharhow <tusharsoftdev@gmail.com>
2024-03-02 08:34:42 -0800

    Issue fix, readme fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,6 @@ buildscript {
     dependencies {
         classpath ("com.android.tools.build:gradle:8.2.2")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-
-
     }
 }
 
